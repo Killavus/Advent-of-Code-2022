74d1fe095a053176917eb67835d0c5c9f40d4899
Killavus <killavus@gmail.com>
2022-12-01 12:38:06 +0100

    Calorie Counting (#1): abstract reading input by using iterator

--- 1-calorie-counting/src/main.rs
+++ 1-calorie-counting/src/main.rs
@@ -5,35 +5,48 @@ use std::io::prelude::*;
 
 type AppResult<T> = Result<T, Box<dyn std::error::Error>>;
 
-fn most_calories_elf_carry(mut reader: impl BufRead) -> AppResult<usize> {
-    let mut line = String::with_capacity(12);
-    let mut current_elf_calories = 0;
-    let mut max_elf_calories = 0;
+struct ElfCarryIter {
+    reader: Box<dyn BufRead>,
+    line: String,
+}
 
-    loop {
-        let bytes_read = reader.read_line(&mut line);
-
-        if let Ok(bytes_read) = bytes_read {
-            if bytes_read == 0 {
-                break;
-            }
-        } else {
-            break;
+impl ElfCarryIter {
+    fn new(reader: impl BufRead + 'static) -> Self {
+        Self {
+            reader: Box::new(reader),
+            line: String::with_capacity(12),
         }
-
-        let content = line.trim();
-        if content.is_empty() {
-            max_elf_calories = max_elf_calories.max(current_elf_calories);
-            current_elf_calories = 0;
-        } else {
-            let calories: usize = content.parse().expect("input should be valid");
-            current_elf_calories += calories;
-        }
-
-        line.truncate(0);
     }
+}
 
-    Ok(max_elf_calories)
+impl Iterator for ElfCarryIter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut elf_calories = 0;
+
+        loop {
+            let bytes_read = self.reader.read_line(&mut self.line);
+            match bytes_read {
+                Ok(0) => break None,
+                Ok(_) => {
+                    let content = self.line.trim();
+                    if content.is_empty() {
+                        break Some(elf_calories);
+                    } else {
+                        let calories: usize = content.parse().expect("input should be valid");
+                        elf_calories += calories;
+                    }
+                    self.line.truncate(0);
+                }
+                Err(_) => break None,
+            }
+        }
+    }
+}
+
+fn most_calories_elf_carry(reader: impl BufRead + 'static) -> AppResult<usize> {
+    Ok(ElfCarryIter::new(reader).max().expect("input is valid"))
 }
 
 fn main() {
