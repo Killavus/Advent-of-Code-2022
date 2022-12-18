e9f01e9b7fe583e24ea59cd558bb09dbff74c143
Killavus <killavus@gmail.com>
2022-12-18 21:23:53 +0100

    Boiling Boulders (#18): implement solution for both parts

--- 18-boiling-boulders/src/main.rs
+++ 18-boiling-boulders/src/main.rs
@@ -70,6 +70,63 @@ impl UnitCube {
     }
 }
 
+type BBox = ((i64, i64), (i64, i64), (i64, i64));
+fn count_exterior_area(start: (i64, i64, i64), bbox: &BBox, cubes: &[UnitCube]) -> i64 {
+    use std::collections::{HashSet, VecDeque};
+
+    let mut queue = VecDeque::new();
+    let mut used = HashSet::new();
+    queue.push_front(start);
+    used.insert(start);
+
+    let mut result = 0;
+    let &((min_x, max_x), (min_y, max_y), (min_z, max_z)) = bbox;
+
+    while !queue.is_empty() {
+        let (x, y, z) = queue.pop_front().unwrap();
+        let cube = UnitCube { x, y, z };
+        for other in cubes.iter() {
+            if other.adjacent(&cube) {
+                result += 1;
+            }
+        }
+
+        for d in [-1, 1] {
+            let x_cube = UnitCube { x: x + d, y, z };
+            if min_x <= x + d
+                && max_x >= x + d
+                && !used.contains(&(x + d, y, z))
+                && !cubes.contains(&x_cube)
+            {
+                queue.push_back((x + d, y, z));
+                used.insert((x + d, y, z));
+            }
+
+            let y_cube = UnitCube { x, y: y + d, z };
+            if min_y <= y + d
+                && max_y >= y + d
+                && !used.contains(&(x, y + d, z))
+                && !cubes.contains(&y_cube)
+            {
+                queue.push_back((x, y + d, z));
+                used.insert((x, y + d, z));
+            }
+
+            let z_cube = UnitCube { x, y, z: z + d };
+            if min_z <= z + d
+                && max_z >= z + d
+                && !used.contains(&(x, y, z + d))
+                && !cubes.contains(&z_cube)
+            {
+                queue.push_back((x, y, z + d));
+                used.insert((x, y, z + d));
+            }
+        }
+    }
+
+    result
+}
+
 fn main() -> Result<()> {
     let cubes = read(BufReader::new(stdin()))?;
 
@@ -89,7 +146,27 @@ fn main() -> Result<()> {
         total += sides;
     }
 
-    println!("Total surface area of the droplet is {}", total);
+    println!("Total area of the droplet is {}", total);
+
+    let min_x = cubes.iter().copied().map(|cube| cube.x).min().unwrap();
+    let max_x = cubes.iter().copied().map(|cube| cube.x).max().unwrap();
+    let min_y = cubes.iter().copied().map(|cube| cube.y).min().unwrap();
+    let max_y = cubes.iter().copied().map(|cube| cube.y).max().unwrap();
+    let min_z = cubes.iter().copied().map(|cube| cube.z).min().unwrap();
+    let max_z = cubes.iter().copied().map(|cube| cube.z).max().unwrap();
+
+    let start = (min_x - 10, min_y - 10, min_z - 10);
+    let exterior_area = count_exterior_area(
+        start,
+        &(
+            (min_x - 10, max_x + 10),
+            (min_y - 10, max_y + 10),
+            (min_z - 10, max_z + 10),
+        ),
+        &cubes,
+    );
+
+    println!("Exterior area of the droplet is {}", exterior_area);
 
     Ok(())
 }
